be41854ca77b1a69ce174e42499e064af1342ae7
Brian Masaki <masakibrian@gmail.com>
2025-02-27 17:40:14 +0300

    updated multiple files

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
-import NavBar from '../components/Navbar'
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import NavBar from '../components/Navbar';
 
 const MainLayout = () => {
   return (
     <>
     <NavBar />
     <Outlet />
+    <ToastContainer />
     </>
   )
 }
